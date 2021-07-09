7ef67de4173ec26151868b8f591f7fe9d7dcf076
Joshua LaPlante <joshualaplante29@yahoo.com>
2021-07-09 11:35:03 -0400

    Initial Check In

---
+++ Tools/BulkInsertForDapperHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using Dapper.Contrib.Extensions;
+
+namespace DataAccess.MsSql.DapperRepository.Tools
+{
+    internal static class BulkInsertForDapperHelper
+    {
+        private static bool IsPropertyMarkedComputed(PropertyDescriptor propertyDescriptor) =>
+            propertyDescriptor.Attributes.OfType<ComputedAttribute>().Any();
+
+        private static bool IsPropertyMarkedKey(PropertyDescriptor propertyDescriptor) =>
+            propertyDescriptor.Attributes.OfType<KeyAttribute>().Any();
+
+
+        public static DataTable ToDataTable<T>(this IEnumerable<T> data)
+        {
+            DataTable table = new DataTable();
+            var properties = TypeDescriptor.GetProperties(typeof(T));
+            var propsValidForInsert = new List<PropertyDescriptor>();
+
+            //get the properties that will be needed for inserts
+            //basically all those without 'computed' or key attributes
+            foreach (PropertyDescriptor prop in properties)
+                if (!IsPropertyMarkedComputed(prop) && !IsPropertyMarkedKey(prop))
+                    propsValidForInsert.Add(prop);
+
+            //add only the valid properties to the data table
+            foreach (var prop in propsValidForInsert)
+                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+
+            //construct the data table
+            foreach (T item in data)
+            {
+                var row = table.NewRow();
+                foreach (var prop in propsValidForInsert)
+                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+
+                table.Rows.Add(row);
+            }
+
+            return table;
+        }
+
+
+        /// <summary>
+        /// Gets the rows copied from the specified SqlBulkCopy object
+        /// </summary>
+        /// <param name="bulkCopy">The bulk copy.</param>
+        /// <returns></returns>
+        public static bool TryGetRowsCopied(this SqlBulkCopy bulkCopy, out int rowsCopied)
+        {
+            FieldInfo rowsCopiedField = null;
+            if (rowsCopiedField == null)
+            {
+                rowsCopiedField = typeof(SqlBulkCopy).GetField("_rowsCopied", BindingFlags.NonPublic | BindingFlags.GetField | BindingFlags.Instance);
+            }
+
+            return int.TryParse(rowsCopiedField?.GetValue(bulkCopy).ToString(), out rowsCopied);
+        }
+
+
+    }
+}
---
+++ Tools/SqlBulkCopyForDapper.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccess.MsSql.DapperRepository.Tools
+{
+    public class SqlBulkCopyForDapper<T> where T : class
+    {
+        public int CommitBatchSize { get; set; } = 1000;
+
+        public int TimeoutInSeconds { get; set; } = 120;
+
+        private readonly string _tableName;
+
+        private bool IsTableMapped { get; set; }
+
+        private List<SqlBulkCopyColumnMapping> TableMapping { get; set; }
+
+        private SqlConnection Connection { get; set; }
+
+
+        public SqlBulkCopyForDapper(SqlConnection connection, string tableName)
+        {
+            _tableName = tableName;
+            IsTableMapped = false;
+            Connection = connection;
+            TableMapping = new List<SqlBulkCopyColumnMapping>();
+        }
+
+        private void MapTable()
+        {
+
+        }
+
+        public async Task<long> BulkCopy(IEnumerable<T> objToInsert)
+        {
+            var entityList = objToInsert.ToList();
+            var taskList = new List<Task<long>>();
+
+            if (entityList.Any())
+            {
+                taskList.Add(DataTransferTask(entityList.ToDataTable()));
+
+                await Task.WhenAll(taskList);
+            }
+
+            return taskList.Sum(x => x.Result);
+        }
+
+        private async Task<long> DataTransferTask(DataTable dt)
+        {
+            long result = 0;
+
+            if (!IsTableMapped)
+                MapObjectToTable(dt);
+
+            var copyOptions = SqlBulkCopyOptions.FireTriggers |
+                              SqlBulkCopyOptions.UseInternalTransaction;
+
+            using (SqlBulkCopy bk = new SqlBulkCopy(Connection, copyOptions, null)
+            {
+                DestinationTableName = _tableName,
+                BulkCopyTimeout = TimeoutInSeconds,
+                BatchSize = CommitBatchSize
+            })
+            {
+                foreach (var map in TableMapping)
+                    bk.ColumnMappings.Add(map);
+
+                //Connection closes because this is in batches
+                if (Connection.State != ConnectionState.Open)
+                    Connection.Open();
+
+                await bk.WriteToServerAsync(dt);
+                result = bk.TryGetRowsCopied(out int rowsCopied) ? rowsCopied : 0;
+
+                bk.Close();
+            }
+
+
+            return result;
+        }
+
+        private void MapObjectToTable(DataTable dt)
+        {
+            foreach (DataColumn col in dt.Columns)
+            {
+                TableMapping.Add(new SqlBulkCopyColumnMapping
+                {
+                    DestinationColumn = col.ColumnName,
+                    SourceColumn = col.ColumnName
+                });
+            }
+
+            IsTableMapped = true;
+        }
+    }
+}
